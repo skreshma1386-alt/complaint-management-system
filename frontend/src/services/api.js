c80e0077df21651a5a485a08d1b19b6a3916c2c1
skreshma1386-alt <skreshma1386@gmail.com>
2026-06-26 16:25:01 +0530

    connect vercel frontend with render complaint-management-system

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-    baseURL: "https://your-render-backend-name.onrender.com"
+    baseURL: "https://complaint-management-system-5qw0.onrender.com"
 });
 
 export default API;
