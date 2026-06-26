17e51143cede5a2a4d22edaf68b31dfcba730521
skreshma1386-alt <skreshma1386@gmail.com>
2026-06-26 10:24:43 +0530

    Use MongoDB Atlas connection string

--- config/db.js
+++ config/db.js
@@ -2,7 +2,8 @@ const mongoose = require("mongoose");
 
 const connectDB = async () => {
   try {
-    await mongoose.connect("mongodb://127.0.0.1:27017/complaintDB");
+    await mongoose.connect(process.env.MONGO_URI);
+
     console.log("MongoDB Connected Successfully");
   } catch (error) {
     console.log("Database Error:", error.message);
